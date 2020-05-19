ae1dca202a3a0be78953eab554d66e73f3d501d4
MeiK <meik2333@gmail.com>
2020-05-19 18:52:09 +0800

    Update

--- src/result.rs
+++ src/result.rs
@@ -1,4 +1,6 @@
+use super::error::Result;
 use std::fmt;
+use std::path::Path;
 
 #[derive(Debug, Copy, Clone)]
 pub struct ResourceUsed {
@@ -18,5 +20,16 @@ pub enum TestCaseResult {
 impl fmt::Display for TestCaseResult {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:?}", self)
+    }
+}
+
+impl TestCaseResult {
+    pub fn standard<P: AsRef<Path>>(output_file: P, answer_file: P) -> Result<TestCaseResult> {
+        let result = TestCaseResult::Accepted(ResourceUsed {
+            time_used: 0,
+            memory_used: 0,
+        });
+        // TODO: 对比答案文件，返回结果
+        Ok(result)
     }
 }
